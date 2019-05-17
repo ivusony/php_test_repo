a440ec229f408622d183f097e6ab1614afe1bbde
Ivan Radulov <ivusony@gmail.com>
2019-05-17 11:16:48 +0200

    greetings.php

--- db_conn.php
+++ db_conn.php
@@ -3,10 +3,10 @@
     
 
     function Open(){
-        $host = "localhost",
-        $db_user = "radulov",
-        $db_pass = "3n1gma86ir",
-        $db = "test_db"
+        $host = "localhost";
+        $db_user = "radulov";
+        $db_pass = "3n1gma86ir";
+        $db = "test_db";
 
         $conn = new mysqli($host, $db_user, $db_pass, $db) or die("Error connecting to db \n". $conn -> error);
 
