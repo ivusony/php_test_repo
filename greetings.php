afbaa2666352327ab54462330b4a0bfa49a2aa57
Ivan Radulov <ivusony@gmail.com>
2019-05-17 10:23:50 +0200

    greetings.php

---
+++ greetings.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+</head>
+<body>
+        <h1>
+            <?php 
+                $name = $_GET['name'];
+                $city = $_GET['city'];
+
+                if (isset($_GET['submit'])) {
+                    
+                    echo 'You are '.$name.' from '.$city.'. Greetings!';
+                }
+            ?>
+        </h1>
+</body>
+</html>
--- index.php
+++ index.php
@@ -11,20 +11,12 @@
 <body>
     <h1>Custom test page served by apache</h1>
     <h3>Served by apache2</h3>
-    <form action="" method="GET">
+    <form action="./greetings.php" method="GET">
         <input type="text" placeholder="Enter your name" name="name">
         <input type="text" placeholder="Enter city" name="city">
         <input type="submit" value="Submit form" name='submit'>
     </form>
 
-    <?php 
-        $name = $_GET['name'];
-        $city = $_GET['city'];
-
-        if (isset($_GET['submit'])) {
-            
-            echo 'You are '.$name.' from '.$city.'. Greetings!';
-        }
-    ?>
+    
 </body>
 </html>
