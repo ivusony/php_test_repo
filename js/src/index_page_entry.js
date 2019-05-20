49d822c2b74e529fbe4ff27fb53daf468ce84ae9
Ivan Radulov <ivusony@gmail.com>
2019-05-20 09:32:43 +0200

    style loaders added

--- js/src/index_page_entry.js
+++ js/src/index_page_entry.js
@@ -1,5 +1,7 @@
 import * as index_controller from '../src/index_page_src/controller';
 
+import * as style from '../src/css/index.css';
+
 setTimeout(function(){
     console.log('works on index page');
 }, 5000)
