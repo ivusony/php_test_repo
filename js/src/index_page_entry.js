b3dbfe30b2517d319b6f3835212cd1759e4e2b9e
Ivan Radulov <ivusony@gmail.com>
2019-05-20 11:58:26 +0200

    css

--- js/src/index_page_entry.js
+++ js/src/index_page_entry.js
@@ -1,6 +1,6 @@
 // import * as index_controller from './index_page_src/controller';
 
-import css from './css/index.css';
+import './css/index.css';
 
 setTimeout(function(){
     console.log('works on index page');
